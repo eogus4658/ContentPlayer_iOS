c576c7bbf05ded276b5a5e37c5da7990823a7e39
이대현 <ej4658@naver.com>
2023-01-09 11:39:50 +0900

    프로젝트 초기 설정

---
+++ ContentPlayer/ContentPlayer/Sources/VCs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContentPlayer
+//
+//  Created by 이대현 on 2023/01/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
